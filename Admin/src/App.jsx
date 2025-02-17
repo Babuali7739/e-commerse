5c4b1be146770ec2fe94fdbb33f8197e54f8ee13
Babuali7739 <129478089+Babuali7739@users.noreply.github.com>
2025-02-17 23:00:19 +0530

    h

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,6 +1,6 @@
 import React,{useState}from 'react'
 import Navbar from './components/Navbar/Navbar'
-import Admin from './pages/admin/Admin'
+import Admin from './pages/Admin/Admin'
 
 const App = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
