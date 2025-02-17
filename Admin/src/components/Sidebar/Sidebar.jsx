9047045dcc0d197fcf588a4a3c55d93a4232df0e
Babuali7739 <129478089+Babuali7739@users.noreply.github.com>
2025-02-17 23:50:24 +0530

    main

--- Admin/src/components/Sidebar/Sidebar.jsx
+++ Admin/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Sidebar.css';
 import {Link} from 'react-router-dom';
-import add_product_icon from '../../assets/Product_Cart';
+import add_product_icon from '../../assets/Product_Cart.svg';
 import list_product_icon from '../../assets/Product_list_icon.svg';
 
 const Sidebar = () => {
